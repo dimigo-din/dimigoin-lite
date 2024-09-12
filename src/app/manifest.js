1defa6d29f0237a0be795808d281d5da2a658695
Seungpyo Suh <me@sspzoa.io>
2024-09-12 17:48:01 +0900

    Update manifest.js

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -4,7 +4,7 @@ export default function manifest() {
     short_name: '디미고인 라이트',
     description:
       '한국디지털미디어고등학교 인트라넷, 디미고인 라이트. 디미고인 라이트는 디미고인의 기능을 최대한 유지하면서도 불필요한 기능을 제거하여 가벼운 사용 경험을 제공합니다.',
-    start_url: '/',
+    start_url: '/login',
     display: 'standalone',
     orientation: 'portrait-primary',
     background_color: '#F6F6FA',
