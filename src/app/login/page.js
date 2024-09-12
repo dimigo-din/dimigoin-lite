653213c201d3367c3fc5a0d044c8721c9a73bfda
Seungpyo Suh <me@sspzoa.io>
2024-09-12 12:24:41 +0900

    fix: loginpage size

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,7 +2,7 @@ import LoginArea from '@/components/login/LoginArea';
 
 export default function Login() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-grow justify-center items-center">
       <LoginArea />
     </div>
   );
