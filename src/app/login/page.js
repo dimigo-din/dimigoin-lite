2a8664a46dab8b0c6f62d4f95ab05f7e83d4907d
Seungpyo Suh <me@sspzoa.io>
2024-09-24 16:34:23 +0900

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,7 +2,7 @@ import LoginArea from '@/components/login/LoginArea';
 
 export default function Login() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-[100dvh]">
       <LoginArea />
     </div>
   );
